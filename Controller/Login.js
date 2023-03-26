bf4aa44abe416a72d4e89253100bab686b19d43c
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-26 19:14:14 +0530

    0.1

--- Controller/Login.js
+++ Controller/Login.js
@@ -1,15 +1,42 @@
 const db = require('../db');
 const Random=require('../Assets/Random');
+const User=require("../Models/User");
 exports.newUser=async(req,res)=>{
     try
     {
-     const u=Random.generateRandomNumber;
-     console.log(u);
-     res.json(
+        var ch=99;
+        while(true && ch>0)
+    {
+     const u=Random.generateRandomNumber();
+     const uid="#player:"+u;
+     const q=await User.find({username:uid});
+     if(q.length===0)
+     {
+        const newUser=new User({
+            username:uid,
+            password:"password"
+        })
+        const result=await newUser.save();
+
+        res.json(
+            {
+                status:"success",
+                uid:uid
+            }
+         )
+         return;
+     }
+     else
+     {
+        ch--;
+     }
+    }
+    res.json(
         {
-            status:"success"
+            status:"something Went wrong"
         }
      )
+     return;
     }
     catch(err)
     {
